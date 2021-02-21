0658f30d21c068dfb31a4685346c724b6db471e3
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2021-02-20 18:58:44 -0800

    CSS changes

--- src/Posts.js
+++ src/Posts.js
@@ -8,10 +8,16 @@ export default function Posts() {
     if (data.posts.length > 0) {
         content = data.posts.map((post, id) => 
             <div className="post-bkg" key={id}>
-                <img src={defaultImage} alt="image of code" className="snippet-img" />
-                <h1 className="post-name">{post.postName}</h1>
-                <p className="post-snippet">{post.snippet + ".."}</p>
-                <Link to={"/post?" + post.postId} className="read-more">Continue Reading</Link>
+                <div className="post-image">
+                    <img src={defaultImage} alt="image of code" className="snippet-img" />
+                </div>
+                <div className="post-content">
+                    <h1 className="post-name">{post.postName}</h1>
+                    <h3 className="post-tags">{post.tags}</h3>
+                    <h5 className="post-date">{post.date}</h5>
+                    <p className="post-snippet">{post.snippet + ".."}</p>
+                    <Link to={"/post?" + post.postId} className="read-more">Continue Reading</Link>
+                </div>
             </div>
         )
     }
